a27f32cd1ef427c2fd616fc1abb904807354c587
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-18 21:51:39 -0500

    all get routes for tag are working as expected

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,12 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: ProductTag }],
+      include: [
+        {
+          model: Product,
+          through: ProductTag
+        }
+      ],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -16,9 +21,28 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [
+        {
+          model: Product,
+          through: ProductTag
+        }
+      ],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tags found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
